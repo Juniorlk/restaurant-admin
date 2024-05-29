20e131405b84ae65d6ce5b31771b7927a8fa9ecb
Juniorlk <juniorlekeu@gmail.com>
2024-05-29 21:40:12 +0100

    listes des commandes

--- app/Models/Commande.php
+++ app/Models/Commande.php
@@ -2,18 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Client;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Categorie extends Model
+class Commande extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'Id_Commande';
 
-    protected $primaryKey = 'Id_Categorie';
-    protected $fillable = ['Nom', 'Description'];
+    protected $fillable = [
+        'Id_Client',
+        'Date_heure',
+        'Mode_paiement',
+    ];
 
-    public function plats()
+    public function client()
     {
-        return $this->hasMany(Plat::class, 'Id_Categorie');
+        return $this->belongsTo(Client::class, 'Id_Client');
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\CommandeController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -15,6 +16,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+
+    //commandes
+    Route::get('commande', [CommandeController::class, 'index'])->name('commande.index');
+
 });
 
 require __DIR__.'/auth.php';
